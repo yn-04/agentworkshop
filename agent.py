3bd4bbc50ac8aee794bbfba56866427633010e1a
yn-04 <methawadee666@gmail.com>
2025-07-11 16:17:11 +0700

    Add files via upload

--- agent.py
+++ agent.py
@@ -1,20 +1,25 @@
-from google.adk.agents.llm_agent import LlmAgent
-from google.adk.tools.mcp_tool.mcp_toolset import MCPToolset
-from google.adk.tools.mcp_tool.mcp_session_manager import StdioServerParameters
+from google.adk.agents import Agent
+from toolbox_core import ToolboxSyncClient
 
-toolset = MCPToolset(
-    connection_params=StdioServerParameters(
-        command='npx', 
-        args=["-y",   
-              "@openbnb/mcp-server-airbnb",
-              "--ignore-robots-txt",
-        ],
-    )
-)
+# Connect to the toolbox server
+toolbox = ToolboxSyncClient("http://127.0.0.1:5000")
 
-root_agent = LlmAgent(
-    name='bnb_agent',
-    model='gemini-2.0-flash',
-    instruction='Help user interact with the AirBNB website and listings.',
-    tools=[toolset], 
+# Load the available tools
+tools = toolbox.load_toolset('basic-toolset')
+
+# Define the agent prompt
+prompt = """
+You are a helpful university student assistant. 
+You handle course searching using tools provided by the university's course management system.
+Answer user questions about courses.
+Do not answer questions about other topics.
+"""
+
+# Create the root agent
+root_agent = Agent(
+    model='gemini-2.0-flash-001',
+    name='course_agent',
+    description='A helpful AI assistant.',
+    instruction=prompt,
+    tools=tools,
 )
